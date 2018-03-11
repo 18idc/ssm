7d02c467ca59dc24c34c18317efadd57498df65a
18idc <admin@q18idc.com>
2018-03-11 14:36:31 +0800

    Initial commit

---
+++ src/main/java/com/q18idc/ssm/dao/StudentMapper.java
@@ -0,0 +1,30 @@
+package com.q18idc.ssm.dao;
+
+import com.q18idc.ssm.entity.Student;
+import com.q18idc.ssm.entity.StudentExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface StudentMapper {
+    long countByExample(StudentExample example);
+
+    int deleteByExample(StudentExample example);
+
+    int deleteByPrimaryKey(Integer sid);
+
+    int insert(Student record);
+
+    int insertSelective(Student record);
+
+    List<Student> selectByExample(StudentExample example);
+
+    Student selectByPrimaryKey(Integer sid);
+
+    int updateByExampleSelective(@Param("record") Student record, @Param("example") StudentExample example);
+
+    int updateByExample(@Param("record") Student record, @Param("example") StudentExample example);
+
+    int updateByPrimaryKeySelective(Student record);
+
+    int updateByPrimaryKey(Student record);
+}
---
+++ src/main/java/com/q18idc/ssm/entity/Student.java
@@ -0,0 +1,43 @@
+package com.q18idc.ssm.entity;
+
+public class Student {
+    private Integer sid;
+
+    private String sname;
+
+    private Integer cid;
+
+    private Classes classes;
+
+    public Classes getClasses() {
+        return classes;
+    }
+
+    public void setClasses(Classes classes) {
+        this.classes = classes;
+    }
+
+    public Integer getSid() {
+        return sid;
+    }
+
+    public void setSid(Integer sid) {
+        this.sid = sid;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname == null ? null : sname.trim();
+    }
+
+    public Integer getCid() {
+        return cid;
+    }
+
+    public void setCid(Integer cid) {
+        this.cid = cid;
+    }
+}
